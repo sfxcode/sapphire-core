e152971e7f062215ecf1318a08f7a1f41b5c0b83
tom <tom@sfxcode.com>
2018-01-04 22:33:28 +0100

    Simple Component Handling added

---
+++ src/it/scala/com/sfxcode/sapphire/core/demo/appdemo/controller/DisplayInfoController.scala
@@ -0,0 +1,9 @@
+package com.sfxcode.sapphire.core.demo.appdemo.controller
+
+import com.sfxcode.sapphire.core.controller.ViewController
+
+class DisplayInfoController extends ViewController {
+  override def didGainVisibility(): Unit = {
+    super.didGainVisibility()
+  }
+}
--- src/it/scala/com/sfxcode/sapphire/core/demo/appdemo/controller/Workspace3Controller.scala
+++ src/it/scala/com/sfxcode/sapphire/core/demo/appdemo/controller/Workspace3Controller.scala
@@ -1,5 +1,20 @@
 package com.sfxcode.sapphire.core.demo.appdemo.controller
 
+import javafx.fxml.FXML
+
+import javafx.scene.layout.StackPane
+import scalafx.Includes._
+
 class Workspace3Controller extends AbstractWorkspaceController {
 
+  @FXML
+  var infoPane: StackPane = _
+
+  lazy val infoController = getController[DisplayInfoController]()
+
+  override def didGainVisibilityFirstTime(): Unit = {
+    super.didGainVisibilityFirstTime()
+    updatePaneContent(infoPane, infoController)
+
+  }
 }
