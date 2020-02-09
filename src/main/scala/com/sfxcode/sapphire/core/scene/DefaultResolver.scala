cd1016144ca2329cdc7517de14ba32eafd8bd3a8
tom <tom@sfxcode.com>
2020-02-09 15:23:50 +0100

    expression language 3 support

--- src/main/scala/com/sfxcode/sapphire/core/scene/DefaultResolver.scala
+++ src/main/scala/com/sfxcode/sapphire/core/scene/DefaultResolver.scala
@@ -9,18 +9,18 @@ class DefaultResolver extends NodePropertyResolving {
 
   def resolve(node: Node): Option[Property[_]] =
     node match {
-      case label: Label => Some(label.textProperty())
-      case text: Text => Some(text.textProperty())
-      case textField: TextField => Some(textField.textProperty())
-      case textArea: TextArea => Some(textArea.textProperty())
-      case datePicker: DatePicker => Some(datePicker.valueProperty())
+      case label: Label               => Some(label.textProperty())
+      case text: Text                 => Some(text.textProperty())
+      case textField: TextField       => Some(textField.textProperty())
+      case textArea: TextArea         => Some(textArea.textProperty())
+      case datePicker: DatePicker     => Some(datePicker.valueProperty())
       case toggleButton: ToggleButton => Some(toggleButton.selectedProperty())
-      case checkBox: CheckBox => Some(checkBox.selectedProperty())
-      case slider: Slider => Some(slider.valueProperty())
-      case comboBox: ComboBox[_] => Some(comboBox.valueProperty())
-      case choiceBox: ChoiceBox[_] => Some(choiceBox.valueProperty())
-      case spinner: Spinner[_] => Some(spinner.getValueFactory.valueProperty())
-      case _ => None
+      case checkBox: CheckBox         => Some(checkBox.selectedProperty())
+      case slider: Slider             => Some(slider.valueProperty())
+      case comboBox: ComboBox[_]      => Some(comboBox.valueProperty())
+      case choiceBox: ChoiceBox[_]    => Some(choiceBox.valueProperty())
+      case spinner: Spinner[_]        => Some(spinner.getValueFactory.valueProperty())
+      case _                          => None
     }
 
 }
